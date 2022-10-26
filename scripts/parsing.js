d0b31ec8363cadf92de32b4794c9f463b56a1823
Zy'Aire McCreary-Scott <zyaire.mccrearyscott@gmail.com>
2022-10-26 15:12:27 -0400

    create parseAndDisplayName functions

--- scripts/parsing.js
+++ scripts/parsing.js
@@ -1,1 +1,31 @@
-console.log('the script tags wrks')
+function parseAndDisplayName(name){
+
+    let space = name.indexOf(' ');
+
+    let firstName = name.substring(0, space);
+    let lastName = name.substring(space+1); 
+    console.log(firstName, lastName, name);
+
+}
+
+
+parseAndDisplayName('Robert DeNiro');
+
+
+// console.log('the script tags wrks')
+
+// let fullName = "Zy'Aire McCreary-Scott";
+
+// console.log(`the variable fullName is ${fullName}`);
+
+// let space = fullName.indexOf(' ');
+
+// console.log(space);
+
+// let firstName = fullName.substring(0, space);
+
+// console.log(firstName);
+
+// let lastName = fullName.substring(space+1);
+
+// console.log(lastName);
